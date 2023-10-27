520b98f5e649e7ea7c36e0d14961ab85a97a1f01
Firuddin <firuddinaliyev888@gmail.com>
2023-10-27 21:40:39 +0400

    contact 2 change

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
+import Navigation from "../../Components/Navigation";
 const Contact = () => {
-  return <div>Contact</div>;
+  return <div>
+    <Navigation/>
+    <h1>contact</h1>
+  </div>;
 };
 export default Contact;
