07b559f02dd5173b7342b02c7f89cef824ff2e49
Firuddin <firuddinaliyev888@gmail.com>
2023-11-01 11:10:19 +0400

    add

--- src/Components/MyRoutes/index.jsx
+++ src/Components/MyRoutes/index.jsx
@@ -1,4 +1,4 @@
-import HomePage from "../../Pages/HomePage"
+import HomePage from "../../Pages/HomePage";
 import ErrorPage from "../../Pages/ErrorPage";
 import Services from "../../Pages/Services";
 import Blog from "../../Pages/Blog";
@@ -6,10 +6,10 @@ import Project from "../../Pages/Project";
 import Contact from "../../Pages/Contact";
 
 export const myRoutes = [
-{ id: 1, path: "/", Element: <HomePage />,title:'Home' },
-{id:2, path:"/services", Element:<Services/>,title:'Services'},
-{id:3, path:"/blog",Element:<Blog/>,title:'Blog'},
-{id:4, path:"/project",Element:<Project/>,title:'Project'},
-{id:5, path:"/contact",Element:<Contact/>,title:'Contact'},
-{id:6, path:"*", Element:<ErrorPage/>},
+  { id: 1, path: "/", Element: HomePage, title: "Home" },
+  { id: 2, path: "/services", Element: Services, title: "Services" },
+  { id: 3, path: "/blog", Element: Blog, title: "Blog" },
+  { id: 4, path: "/project", Element: Project, title: "Project" },
+  { id: 5, path: "/contact", Element: Contact, title: "Contact" },
+  { id: 6, path: "*", Element: ErrorPage },
 ];
