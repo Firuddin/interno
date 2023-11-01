957a35ed60fc6e9e08658b5a3a287b5f279da533
Firuddin <firuddinaliyev888@gmail.com>
2023-11-01 11:09:36 +0400

    add  c

--- src/Components/Routing/index.jsx
+++ src/Components/Routing/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import { myRoutes } from "../MyRoutes";
-const Routing = () => {
+import HomePage from "../../Pages/HomePage";
+import ErrorPage from "../../Pages/ErrorPage";
+import Services from "../../Pages/Services";
+import Blog from "../../Pages/Blog";
+import Project from "../../Pages/Project";
+import Contact from "../../Pages/Contact";
+
+const WebRouting = () => {
   return (
-      <Routes>
-      {
-        myRoutes.map(({id,path,Element})=>{
-          return(
-            <Route key={id} path={path} element={Element}/>
-          
-        )})
-      }
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 };
-export default Routing;
+export default WebRouting;
+
+// {myRoutes.map(({ id, path, Element }) => {
+//   return <Route key={id} path={path} element={<Element />} />;
+//   })}
